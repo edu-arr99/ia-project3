ded6e54a447c76150eeecb0a9823706bbfb90548
Noemi RC <noemi.ramirez@utec.edu.pe>
2023-06-02 21:25:30 -0500

    :sparkles: instalacion de librerias armadillo y boost

--- classes/layer.cpp
+++
@@ -1,3 +0,0 @@
-//
-// Created by NOEMI RAMIREZ on 6/2/23.
-//
--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <boost/version.hpp>
+#include <armadillo>
+
+
+
+//
+//using namespace arma;
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Boost version: " << BOOST_LIB_VERSION << std::endl;
+    arma::vec v(3); // Crear un vector de Armadillo
+
+    v << 1 << 2 << 3; // Asignar valores al vector
+
+    std::cout << "Vector: " << v << std::endl; // Imprimir el vector
+
     return 0;
 }
